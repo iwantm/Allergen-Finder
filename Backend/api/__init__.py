e2893841c6a2828c9e30be216d289f9757c8ead1
Iwan Moreton <me@iwanmoreton.com>
2021-06-14 15:40:29 +0100

    hotfix - remove hard coded secret keys

--- Backend/api/__init__.py
+++ Backend/api/__init__.py
@@ -8,10 +8,10 @@ from flask_jwt_extended import JWTManager
 app = Flask(__name__)
 app.config.update(
     DEBUG=True,
-    SECRET_KEY='acvs',
+    SECRET_KEY=os.getenv('SECRET_KEY'),
     SQLALCHEMY_DATABASE_URI=os.getenv('DATABASE_URL'),
     SQLALCHEMY_TRACK_MODIFICATIONS=False,
-    JWT_SECRET_KEY='t1NP63m4wnBg6nyHYKfmc2TpCOGI4nss'
+    JWT_SECRET_KEY=os.getenv('JWT_SECRET_KEY')
 )
 
 
